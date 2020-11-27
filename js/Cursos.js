3304c26bba985ec64c24022201e482090ddc515b
Manu Molina <maldana261200@gmail.com>
2020-11-26 21:06:43 -0600

    Error en js Curso Tengo un error con el archivo del curso, necesito de su ayuda para solucionarlo :c

---
+++ js/Cursos.js
@@ -0,0 +1,16 @@
+let Curso = {
+    "id" : 0,
+    "nombre_curso" : "",
+    "nombre_docente" : ""
+};
+
+
+function mostrarNombreCurso(nombre){ 
+    console.log(`El nombre del curso es ${nombre}`)
+}
+
+mostrarPrecio = (precio)=>{
+    //Muestra consola en formato de tabla, pero no esta bien
+    //Necesito ayuda
+    console.table(precio);
+} 
